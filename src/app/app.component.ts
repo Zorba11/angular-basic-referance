497a2017a67fd21e0b0ce207c8eb550d0d1d6d3f
Zorba11 <zorbathegreek111@gmail.com>
2022-01-20 16:09:52 -0600

    angular basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,44 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  template: `<p>Hello World!</p>`,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // styles: []
 })
+
 export class AppComponent {
-  title = 'basics';
+name = "alen george"
+images = [
+  'https://picsum.photos/id/237/500/300',
+  'https://picsum.photos/id/237/500/300',
+  'https://picsum.photos/id/237/500/300'
+]
+
+imgUrl = 'https://picsum.photos/id/237/500/300'
+currentDate = new Date()
+cost = 2000
+temperature = 25.3
+pizza = {
+  toppings: ['pepperoni', 'pineapple'],
+  size: 'large'
+}
+
+getName() {
+  return this.name;
+}
+
+changeImage(e: KeyboardEvent) {
+  this.imgUrl = (e.target as HTMLInputElement).value;
+}
+
+logImg(event: string) {
+  console.log(event);
+
+}
+
+blueClass = false;
+
+fontSize = 16
+
 }
